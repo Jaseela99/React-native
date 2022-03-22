771d8daeae98df792b6a7a491ea35694ab6031e8
Jaseela99 <jaseelachittur@gmail.com>
2022-03-22 15:57:37 +0530

    added text

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -3,9 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 //we use safeareaview to veiw away from the notch of the phone
-  return (
-    <View style={styles.container}>
-      <Text>Hey Jaseela here!!</Text>
+
+//handlepress
+const handlePress =()=>{
+  console.log("text pressed")
+}
+return (
+  /* View is designed to be nested inside other views and can have 0 to many children of any type. */
+  <View style={styles.container}>
+     {/*  A React component for displaying text.Text supports nesting, styling, and touch handling. */}
+      <Text onPress={handlePress}>Hey Jaseela here!!</Text>
+      <Text numberOfLines={1}> how u guys doing?? hope you are good..it is summer here..i hate summers..</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "pink"
+    backgroundColor: "pink",
+    alignItems:"center",
+    justifyContent:"center"
   },
 });
