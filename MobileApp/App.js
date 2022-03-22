da44782c5df623a0fe0d559ddd6eca66c7eebe6b
Jaseela99 <jaseelachittur@gmail.com>
2022-03-23 00:14:10 +0530

    added flexbox

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -2,37 +2,82 @@ import { StatusBar } from "expo-status-bar";
 import {
  // Dimensions,
   StyleSheet,
-  Text,
+  //Text,
   View,
-  Image,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
-  Platform
+  // Image,
+  // TouchableHighlight,
+  // TouchableNativeFeedback,
+  // Button,
+  // Alert,
+  // Platform
 } from "react-native";
 
-import { useDimensions,useDeviceOrientation } from "@react-native-community/hooks";
+//import { useDimensions,useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
 //if we have diff orientations we use useDimensions
- console.log(useDimensions())
-const {landscape} = useDeviceOrientation()
+// console.log(useDimensions())
+//const {landscape} = useDeviceOrientation()
 
   //to get dimensions of application device
   //console.log(Dimensions.get("screen"))
 
 
   //handlepress
-  const handlePress = () => {
-    alert("text pressed");
-  };
+  //const handlePress = () => {
+    //alert("text pressed");
+  //};
   return (
+
+    //flexbox
+<View style={{backgroundColor:"white",
+flex:1, 
+//by default it is in column
+flexDirection:"row",//so horizontal is the main axis and vertical is the secondary one
+justifyContent:"center",//aligns main axis to center
+alignItems:"center",//aligns secondary axis to center
+flexWrap:"wrap",
+alignContent:"center"//when we have wrapping we use aligncontent instead of alignitems
+}}>
+
+<View style={{backgroundColor:"gold",
+//flex:1 or -1 as flexgrow and flexshrink
+flexBasis:100 ,//represents width or height acorrding to main axis is horizontal or vertical
+//width:100,
+height:100,
+//to align a specific veiw alignSelf:"flex-start"
+}}/>
+<View style={{backgroundColor:"tomato",
+//flex:1
+width:100,
+height:100}}/>
+<View style={{backgroundColor:"dodgerblue",
+//flex:1
+width:100,
+height:100,
+}}/>
+<View style={{backgroundColor:"grey",
+//flex:1
+width:100,
+height:100,
+}}/>
+<View style={{backgroundColor:"greenyellow",
+//flex:1
+width:100,
+height:100,
+}}/>
+
+
+  </View>
     /* View is designed to be nested inside other views and can have 0 to many children of any type.
     style can be an arry which container more than one stylesheets*/
-    <View style={[styles.container,containerStyle]}>
-      {/*  A React component for displaying text.Text supports nesting, styling, and touch handling. */}
-      <Text onPress={handlePress}>Hey Jaseela here!!</Text>
+
+    /* <View style={[styles.container,containerStyle]}> */
+
+
+      /*  A React component for displaying text.Text supports nesting, styling, and touch handling. */
+     
+      /* <Text onPress={handlePress}>Hey Jaseela here!!</Text>
       <Text numberOfLines={1}>
         {" "}
         how u guys doing?? hope you are good..it is summer here..i hate
@@ -46,13 +91,19 @@ const {landscape} = useDeviceOrientation()
           {text:"yes", onPress:()=>console.log("yes")},
           {text:"no", onPress:()=>console.log("no")}
         ] )}
-      />
-      {/* to add image from folder
-    <Image source={require("./assets/icon.png")}/> */}
+      /> */
 
-      {/* to add network image */}
-      {/* image components doesnt have on press so we use touchables */}
-      <TouchableHighlight
+
+      /* to add image from folder
+    <Image source={require("./assets/icon.png")}/> */
+
+
+
+      /* to add network image */
+      /* image components doesnt have on press so we use touchables */
+
+
+      /* <TouchableHighlight
         onPress={() => {
           alert("image tapped");
         }}
@@ -68,14 +119,15 @@ const {landscape} = useDeviceOrientation()
             uri: "https://picsum.photos/200/300",
           }}
         />
-      </TouchableHighlight>
+      </TouchableHighlight> */
 
-      <TouchableNativeFeedback
+
+
+     /*  <TouchableNativeFeedback
         onPress={() => {
           console.log("tapped");
         }}
       >
-       {/*  inline styling */}
         <View
           style={{
             width: 150,
@@ -84,20 +136,21 @@ const {landscape} = useDeviceOrientation()
           }}
         ></View>
       </TouchableNativeFeedback>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar style="auto" /> */
+    //</View>
+    
   );
 }
 //creates a stylesheet with reference to an obj,which validates the properties
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:"pink",
-    height:"100%",
-    weight:landscape? "100%":"30%",
-  //  Returns an object which contains all available common and specific constants related to the platform.
-    paddingTop:Platform.OS==="android" ? StatusBar.currentHeight:0
-  },
-});
-//it over writes the bg color in styles.container
-const containerStyle ={backgroundColor:"orange"}
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor:"pink",
+//     height:"100%",
+//     weight:landscape? "100%":"30%",
+//   //  Returns an object which contains all available common and specific constants related to the platform.
+//     paddingTop:Platform.OS==="android" ? StatusBar.currentHeight:0
+//   },
+// });
+// //it over writes the bg color in styles.container
+// const containerStyle ={backgroundColor:"orange"}
